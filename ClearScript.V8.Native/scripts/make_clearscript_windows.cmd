570560e7a141040339346010c772ece09c1b739e
Clark Yang <yangpc.china@gmail.com>
2021-02-24 20:04:00 +0800

    Fixed a bug in scripts.

--- ClearScript.V8.Native/scripts/make_clearscript_windows.cmd
+++ ClearScript.V8.Native/scripts/make_clearscript_windows.cmd
@@ -8,7 +8,7 @@ echo "Building ClearScript.V8.Native(x86) ..."
 echo ================================ 
 setlocal
 set CLEARSCRIPT_V8_BUILD_DIR=%CLEARSCRIPT_V8_SRC_DIR%\build\x86
-cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_INSTALL_PREFIX=${INSTALL_ROOT_DIR} -S %CLEARSCRIPT_V8_SRC_DIR% -B %CLEARSCRIPT_V8_BUILD_DIR%
+cmake -G "Visual Studio 16 2019" -A Win32 -DCMAKE_INSTALL_PREFIX=%INSTALL_ROOT_DIR% -S %CLEARSCRIPT_V8_SRC_DIR% -B %CLEARSCRIPT_V8_BUILD_DIR%
 cmake --build %CLEARSCRIPT_V8_BUILD_DIR% --config Release --target install 
 
 endlocal
@@ -17,7 +17,7 @@ echo "Building ClearScript.V8.Native(x64) ..."
 echo ================================ 
 setlocal
 set CLEARSCRIPT_V8_BUILD_DIR=%CLEARSCRIPT_V8_SRC_DIR%\build\x64
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=${INSTALL_ROOT_DIR} -S %CLEARSCRIPT_V8_SRC_DIR% -B %CLEARSCRIPT_V8_BUILD_DIR%
+cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_INSTALL_PREFIX=%INSTALL_ROOT_DIR% -S %CLEARSCRIPT_V8_SRC_DIR% -B %CLEARSCRIPT_V8_BUILD_DIR%
 cmake --build %CLEARSCRIPT_V8_BUILD_DIR% --config Release --target install 
 
 endlocal
